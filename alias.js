801579ef7071722a3570cc3c60272f8087367334
jadhavmayur72 <jadhav.mayur@thinksys.com>
2022-04-19 12:46:52 +0530

    alias

---
+++ alias.js
@@ -0,0 +1,5 @@
+var a = 465;
+var b = "hello world";
+var c = false;
+var d = undefined;
+console.warn(a, b, c, d);
---
+++ alias.ts
@@ -0,0 +1,8 @@
+type dataType = string | number | boolean  | null
+
+
+let a:dataType=465
+let b:dataType="hello world"
+let c:dataType=false
+let d:dataType=undefined;
+console.warn(a,b,c,d)
